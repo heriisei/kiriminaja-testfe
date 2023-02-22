e02d938a1c9e69c4ca42c75a8d88229f3c3cefba
Heri Risnanto <heri.risnanto@gmail.com>
2023-02-22 21:08:29 +0700

    chore: install unocss with its icon & variantGroup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,21 @@
+import { transformerVariantGroup } from 'unocss';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   typescript: {
     shim: false
-  }
+  },
+  modules: [
+    '@unocss/nuxt',
+  ],
+  unocss: {
+    // presets
+    uno: true, // enabled `@unocss/preset-uno`
+    // or
+    // wind: true, // enabled `@unocss/preset-wind`
+    icons: true,
+    transformers: [
+			transformerVariantGroup()
+		],
+  },
 })
