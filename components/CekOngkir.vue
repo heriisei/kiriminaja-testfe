c0f6d34aa891827d3fddea2c0faa7fcf268b72ce
Heri Risnanto <heri.risnanto@gmail.com>
2023-02-23 22:23:04 +0700

    feat: fixing ekspedisi autoselect field

--- components/CekOngkir.vue
+++ components/CekOngkir.vue
@@ -1,4 +1,17 @@
 <script setup>
+import { storeToRefs } from 'pinia';
+import { useEkspedisiStore } from '~~/store/ekspedisi';
+
+const ekspedisi = useEkspedisiStore()
+const { kelurahan } = storeToRefs(ekspedisi)
+const { getKelurahan } = ekspedisi
+const onSearch = (keyword) => {
+  getKelurahan(keyword)
+}
+const renderLabel = (kelurahan) => kelurahan ? kelurahan.text : ''
+const loading = ref(false)
+
+const selectedKelurahan = ref('')
 </script>
 <template>
   <section>
@@ -6,13 +19,8 @@
       Cek Ongkir Ekspedisi
     </h2>
     <form class="p-1.25rem bg-$bg-muted rounded-1.5rem">
-      <i-select
-          v-model="selectedKelurahan"
-          :options="filteredKelurahan"
-          autocomplete
-          placeholder="Choose something.."
-          @search="onSearch"
-      />
+      <i-select v-model="selectedKelurahan" :options="kelurahan" :label="renderLabel" :loading="loading" autocomplete
+        placeholder="Choose something.." @search="onSearch" />
       <button type="submit" class="flex gap-.75rem justify-center w-full bg-$main-brand-color p-1rem rounded-full">
         <div class="i-tabler:search text-1.5rem"></div>
         Cek Ongkir
